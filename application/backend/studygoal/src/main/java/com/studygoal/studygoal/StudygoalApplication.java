2d5555a6dd1f691cb01fdcf7cf978100988d0075
LauroM <lauromilagres@gmail.com>
2022-06-12 11:09:06 -0300

    back

---
+++ application/backend/studygoal/src/main/java/com/studygoal/studygoal/StudygoalApplication.java
@@ -0,0 +1,13 @@
+package com.studygoal.studygoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudygoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudygoalApplication.class, args);
+	}
+
+}
